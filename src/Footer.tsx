487035f94d96c3a4d78ab536efe289f9d5aa79fb
jwaszkie <jedrzej.waszkiewicz@capgemini.com>
2026-05-18 14:26:00 +0200

    badges

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,3 +1,4 @@
+import { PartnersModalTrigger } from "./PartnersModalTrigger.tsx";
 import { useLang } from "./lang/LangContext";
 import "./Footer.css";
 
@@ -33,6 +34,7 @@ export const Footer = () => {
             all 16 provinces and over 470 types of medical services.
           </p>
           <p>Source data comes from the official portal maintained by NFZ.</p>
+          <PartnersModalTrigger />
         </article>
       </footer>
     );
@@ -68,6 +70,7 @@ export const Footer = () => {
         <p>
           Dane źródłowe pochodzą z oficjalnego portalu prowadzonego przez NFZ.
         </p>
+        <PartnersModalTrigger />
       </article>
     </footer>
   );
---
+++ src/PartnerBadges.tsx
@@ -0,0 +1,14 @@
+export const PartnerBadges = () => {
+  return (
+    <>
+      <a href="https://uno.directory" target="_blank" rel="noopener">
+        <img
+          src="https://uno.directory/uno-directory.svg"
+          alt="Listed on Uno Directory"
+          width="120"
+          height="30"
+        />
+      </a>
+    </>
+  );
+};
